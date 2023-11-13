cc7eea70a116a97323178df75b87b30af9afdeec
hitorilabs <131238467+hitorilabs@users.noreply.github.com>
2023-11-13 15:29:40 -0500

    screwed up the order

--- .config/nvim/lua/main/remap.lua
+++ .config/nvim/lua/main/remap.lua
@@ -1,3 +1,12 @@
+vim.g.mapleader = " "
+
+-- regular mappings
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>d", vim.cmd.Ex, { desc = "Open Explorer" })
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
+vim.keymap.set("n", "J", "j", { desc = "unmap bc conflict with tmux movement" })
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -8,10 +17,3 @@ vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, {})
 
--- regular mappings
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>d", vim.cmd.Ex, { desc = "Open Explorer" })
-vim.keymap.set("n", "<leader>w", vim.cmd.w)
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
-vim.keymap.set("n", "J", "j", { desc = "unmap bc conflict with tmux movement" })
