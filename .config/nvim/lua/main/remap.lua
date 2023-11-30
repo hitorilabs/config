81dc967ab914079f50696ad307cb4ebb0451dc19
bocchi <131238467+hitorilabs@users.noreply.github.com>
2023-11-30 16:36:13 -0500

    add find hidden files config

--- .config/nvim/lua/main/remap.lua
+++ .config/nvim/lua/main/remap.lua
@@ -9,10 +9,15 @@ vim.keymap.set("n", "J", "j", { desc = "unmap bc conflict with tmux movement" })
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fh', function()
+    builtin.find_files({
+        find_command = {"rg", "--files", "--hidden", "--glob", "!.git/*"}
+    })
+end, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-f>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>hh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, {})
